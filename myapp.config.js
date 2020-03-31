b282cca48765abadc0e8f7d4ca07d0e8b5a9fa1a
leolurunhe <runhelu@usc.edu>
2020-03-31 10:55:59 -0700

    1

---
+++ myapp.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    apps : [{
+      name        : "myapp-api",
+      script      : "./server/server.js",
+      watch       : true,
+      env: {
+        "NODE_ENV": "development",
+      },
+      env_production : {
+       "NODE_ENV": "production"
+      }
+    },
+    {
+      name       : "myapp-client",
+      script     : "./client/server.js",
+      watch       : true,
+      env: {
+        "NODE_ENV": "development",
+      },
+      env_production : {
+        "NODE_ENV": "production"
+      }
+    }]
+  }
